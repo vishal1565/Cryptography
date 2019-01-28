e9051582dc648f35692794d532faab72d6734a68
Vishal Vishwakarma <vishal.1565@gmail.com>
2019-01-28 10:43:03 +0530

    Add files via upload

---
+++ SubstitutionCipher/decrypt.py
@@ -0,0 +1,5 @@
+# Python code for Substitution Cipher Decryption
+# Save the key in file 'key.txt'
+# Save Cipher Text in 'Output.txt'
+# Output of the CipherText will be Saved in 'DecryptedText.txt'
+
